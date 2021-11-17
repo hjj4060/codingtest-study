243a297ef2fa97f8ec8cdcf8ed00647eb4bc42aa
hjj4060 <rkdgustn77@naver.com>
2021-11-18 02:27:51 +0900

    chatAt 문제풀이

---
+++ src/basic/string/ToCharArray.java
@@ -0,0 +1,24 @@
+package basic.string;
+
+/*
+Problem
+Input: String s = "in(f(lea)r)n)";
+Output: "in(f(lea)r)n"
+Input: String s = “(a(b(c)d)"; Output: “(a(bc)d)”
+
+Note
+Input: s = "a)b(c)d" Output: "ab(c)d"
+Input: s = “))((" Output:""
+최소 괄호의 수를 제거하여 '(' ')' 처럼 유효한 parentheses string 리턴 1. 빈 문자열이거나 소문자 만 포함
+ */
+public class ToCharArray {
+    public static void main(String[] args) {
+
+        System.out.println( new ToCharArray().solve() );
+    }
+
+    public String solve() {
+
+        return null;
+    }
+}
