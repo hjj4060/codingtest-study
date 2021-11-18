86bbc3d7eaf02ed86af97b07603532d480086fb0
hjj4060 <rkdgustn77@naver.com>
2021-11-19 06:19:02 +0900

    baekjoon

---
+++ src/baekjoon/stepbystep/if_statement/CompareTwoNumbers.java
@@ -0,0 +1,19 @@
+package baekjoon.stepbystep.if_statement;
+
+import java.util.Scanner;
+
+/*
+    https://www.acmicpc.net/problem/1330
+
+    두 수 A,B 를 입력하면 첫째 줄에 다음 세 가지 중 하나를 출력한다.
+    - A가 B보다 큰 경우에는 '>'를 출력한다.
+    - A가 B보다 작은 경우에는 '<'를 출력한다.
+    - A와 B가 같은 경우에는 '=='를 출력한다.
+ */
+public class CompareTwoNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
