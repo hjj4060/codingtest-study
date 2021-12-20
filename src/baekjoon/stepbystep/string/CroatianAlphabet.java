f4004d8f13b11e547f87c8a245d5840cf219d690
hjj4060 <rkdgustn77@naver.com>
2021-12-20 21:45:24 +0900

    baekjoon

---
+++ src/baekjoon/stepbystep/string/CroatianAlphabet.java
@@ -0,0 +1,61 @@
+package baekjoon.stepbystep.string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class CroatianAlphabet {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String table[] = new String[]{"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        String s = st.nextToken();
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        Loop1 :
+        for(int i=0; i<s.length(); i++) {
+            String s1 = s.substring(i, i + 1);
+            String s2 = null;
+            String s3 = null;
+
+            if (i < s.length() - 1) {
+                s2 = s.substring(i, i + 2);
+            }
+            if (i < s.length() - 2) {
+                s3 = s.substring(i, i + 3);
+            }
+
+            //3자리수 비교
+            if(s3 != null) {
+                for (String t : table) {
+                    if (t.equals(s3)) {
+                        //set.add(s3);
+                        arrayList.add(s3);
+                        i = i + 2;
+                        continue Loop1;
+                    }
+                }
+            }
+
+            //2자리수 비교
+            if(s2 != null) {
+                for (String t : table) {
+                    if (t.equals(s2)) {
+                        //set.add(s2);
+                        arrayList.add(s2);
+                        i = i + 1;
+                        continue Loop1;
+                    }
+                }
+            }
+            //set.add(s1);
+            arrayList.add(s1);
+        }
+
+        System.out.println(arrayList.size());
+    }
+}
--- src/baekjoon/stepbystep/string/Dial.java
+++ src/baekjoon/stepbystep/string/Dial.java
@@ -1,8 +1,67 @@
 package baekjoon.stepbystep.string;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
 //https://www.acmicpc.net/problem/5622
 public class Dial {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        String s = st.nextToken().toUpperCase(Locale.ROOT);
+
+        int time = 0;
+
+        for (char c : s.toCharArray()) {
+            switch(c) {
+                case 'A':
+                case 'B':
+                case 'C':
+                    time += 3;
+                    break;
+                case 'D':
+                case 'E':
+                case 'F':
+                    time += 4;
+                    break;
+                case 'G':
+                case 'H':
+                case 'I':
+                    time += 5;
+                    break;
+                case 'J':
+                case 'K':
+                case 'L':
+                    time += 6;
+                    break;
+                case 'M':
+                case 'N':
+                case 'O':
+                    time += 7;
+                    break;
+                case 'P':
+                case 'Q':
+                case 'R':
+                case 'S':
+                    time += 8;
+                    break;
+                case 'T':
+                case 'U':
+                case 'V':
+                    time += 9;
+                    break;
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
+                    time += 10;
+                    break;
+            }
+        }
+        System.out.println(time);
     }
 }
