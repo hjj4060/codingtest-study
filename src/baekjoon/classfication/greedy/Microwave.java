9da56f7e1bd396d5d90c2a3e6b9720577506554c
HyunSu, Kang <rkdgustn77@naver.com>
2022-02-12 18:02:19 +0900

    initial

--- src/baekjoon/classfication/greedy/Microwave.java
+++ src/baekjoon/classfication/greedy/Microwave.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+//https://www.acmicpc.net/problem/10162
 public class Microwave {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
