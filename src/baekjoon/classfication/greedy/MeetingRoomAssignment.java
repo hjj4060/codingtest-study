eafad4979e3a60ab53b682fa188ffeceffda9607
kang <rkdgustn77@naver.com>
2022-02-17 23:04:05 +0900

    initial

--- src/baekjoon/classfication/greedy/MeetingRoomAssignment.java
+++ src/baekjoon/classfication/greedy/MeetingRoomAssignment.java
@@ -1,8 +1,52 @@
 package baekjoon.classfication.greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 //https://www.acmicpc.net/problem/1931
 public class MeetingRoomAssignment {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int time[][] = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            time[i][0] = Integer.parseInt(st.nextToken());
+            time[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+
+                return o1[1] - o2[1];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(time[i][0] + " " + time[i][1]);
+        }
+
+        int count = 0;
+        int prev_end_time = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (prev_end_time <= time[i][0]) {
+                prev_end_time = time[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
