fb28387d11bc39f175693c41729183b02dccbeaf
kang <rkdgustn77@naver.com>
2022-02-22 23:26:38 +0900

    initial

--- src/baekjoon/classfication/greedy/Rope.java
+++ src/baekjoon/classfication/greedy/Rope.java
@@ -3,6 +3,7 @@ package baekjoon.classfication.greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 //https://www.acmicpc.net/problem/2217
@@ -11,6 +12,22 @@ public class Rope {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int ropes[] = new int[N];
 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            ropes[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(ropes);
+
+        int result = ropes[N - 1];
+
+        for (int i = 0; i < ropes.length; i++) {
+            result = Math.max(result, ropes[i] * (N - i));
+        }
+
+        System.out.println(result);
     }
 }
