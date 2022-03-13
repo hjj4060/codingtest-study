c364493d8ecb10edbd58b91644724e750214606f
kang <rkdgustn77@naver.com>
2022-03-14 03:55:31 +0900

    initial

---
+++ src/baekjoon/classfication/dp/Tiling2XN.java
@@ -0,0 +1,9 @@
+package baekjoon.classfication.dp;
+
+//https://www.acmicpc.net/problem/11726
+public class Tiling2XN {
+    public static void main(String[] args) {
+        long a = 9 % 10007;
+        System.out.println(a);
+    }
+}
