113f1823673ad453bbf126e9ef4c2aafa31ef99b
hjj4060 <rkdgustn77@naver.com>
2022-04-06 23:06:05 +0900

    baekjoon

--- src/baekjoon/solvedac/silver1/MazeExploration.java
+++ src/baekjoon/solvedac/silver1/MazeExploration.java
@@ -14,11 +14,11 @@ public class MazeExploration {
     static int dx[] = {1,0,-1,0};
     static int dy[] = {0,1,0,-1};
 
-    static final int Y = 4 + 1;
-    static final int X = 6 + 1;
+    static final int X = 0;
+    static final int Y = 0;
 
-    static Boolean visit[][] = new Boolean[Y][X];
-    static int maze[][] = new int[Y][X];
+    static Boolean visit[][];
+    static int maze[][];
 
     static Queue<Node> q = new LinkedList<>();
 
@@ -26,26 +26,22 @@ public class MazeExploration {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); //도착점 가로
-        int M = Integer.parseInt(st.nextToken()); //도착점 세로
+        int Y = Integer.parseInt(st.nextToken()) + 1; //도착점 세로 5
+        int X = Integer.parseInt(st.nextToken()) + 1; //도착점 가로 7
 
-        for (int i = 1; i < Y; i++) {
+        visit = new Boolean[Y][X];
+        maze = new int[Y][X];
+
+        for (int i = 1; i < maze.length; i++) {
             st = new StringTokenizer(br.readLine());
             String s = st.nextToken();
 
             for (int j = 0; j < s.length(); j++) {
-                maze[i][j+1] = s.charAt(j) - '0';
+                maze[i][j + 1] = s.charAt(j) - '0';
             }
         }
 
-        //test
-        for (int i = 1; i < maze.length; i++) {
-            for (int j = 1; j < maze[i].length; j++) {
-                System.out.print(maze[i][j]);
-            }
-            System.out.println();
-        }
-
+        test();
 
         visit[1][1] = true;
         q.add(new Node(1,1));
@@ -53,14 +49,10 @@ public class MazeExploration {
         BFS();
         System.out.println();
 
-        //test
-        for (int i = 1; i < maze.length; i++) {
-            for (int j = 1; j < maze[i].length; j++) {
-                System.out.print(maze[i][j]);
-            }
-            System.out.println();
-        }
+        test();
+
     }
+
 
     private static void BFS() {
         Node n;
@@ -89,6 +81,15 @@ public class MazeExploration {
         public Node(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+
+    private static void test() {
+        for (int i = 1; i < maze.length; i++) {
+            for (int j = 1; j < maze[i].length; j++) {
+                System.out.print(maze[i][j]);
+            }
+            System.out.println();
         }
     }
 }
