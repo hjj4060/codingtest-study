061fe086f5136ca9c96893f32d383ae380904611
hjj4060 <rkdgustn77@naver.com>
2022-04-15 23:08:44 +0900

    baekjoon

--- src/baekjoon/solvedac/silver1/HideAndSeek.java
+++ src/baekjoon/solvedac/silver1/HideAndSeek.java
@@ -1,8 +1,25 @@
 package baekjoon.solvedac.silver1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 //https://www.acmicpc.net/problem/1697 숨바꼭질
 public class HideAndSeek {
-    public static void main(String[] args) {
+    static int N, K;
 
+    public static void main(String[] args) throws IOException {
+        input();
+
+
+    }
+
+    static private void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
     }
 }
