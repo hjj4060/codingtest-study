e5feb12bdb22feacb2445ecfb507eb11a2ebe280
kang <rkdgustn77@naver.com>
2022-05-01 15:58:44 +0900

    Changes

--- src/baekjoon/solvedac/silver1/BaekJoon2156.java
+++ src/baekjoon/solvedac/silver1/BaekJoon2156.java
@@ -1,8 +1,18 @@
 package baekjoon.solvedac.silver1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 //https://www.acmicpc.net/problem/2156 포도주 시식
 public class BaekJoon2156 {
-    public static void main(String[] args) {
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
 
     }
 }
--- src/baekjoon/solvedac/silver1/RGBDistance.java
+++ src/baekjoon/solvedac/silver1/RGBDistance.java
@@ -8,31 +8,61 @@ import java.util.StringTokenizer;
 //https://www.acmicpc.net/problem/1149 RGB거리
 public class RGBDistance {
     static int house[][];
-    static int dp[][];
+    static Integer dp[][];
+    static int N;
 
     public static void main(String[] args) throws IOException{
         input();
+        dp();
 
+        //test();
 
+        System.out.println(Math.min(Math.min(dp[N - 1][0], dp[N - 1][1]), dp[N - 1][2]));
+    }
 
+    private static void dp() {
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1] + house[i][0] , dp[i - 1][2] + house[i][0]);
+            dp[i][1] = Math.min(dp[i - 1][0] + house[i][1] , dp[i - 1][2] + house[i][1]);
+            dp[i][2] = Math.min(dp[i - 1][0] + house[i][2] , dp[i - 1][1] + house[i][2]);
+        }
     }
 
     private static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
         house = new int[N][3];
+        dp = new Integer[N][3];
 
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
 
+            for (int j = 0; j < 3; j++) {
+                int n = Integer.parseInt(st.nextToken());
+                house[i][j] = n;
+                if (i == 0) {
+                    dp[i][j] = n;
+                }
+            }
+        }
+    }
+
+    private static void test() {
+        for (int i = 0; i < house.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(house[i][j] + " ");
+            }
+            System.out.println();
         }
 
-        //dp[0] = house[0] 초기화
-
-        /*
-            dp[0][0] 선택시 dp[1] = dp[
-         */
+        for (int i = 0; i < house.length; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
