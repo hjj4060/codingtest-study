c2486905d6063ca6851b06b3c2ab7e646f7f7df7
hjj4060 <rkdgustn77@naver.com>
2023-11-19 03:35:48 +0900

    백준 문제 스타트와 링크

---
+++ src/backjoon_14889.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+//https://www.acmicpc.net/problem/14889 스타트와 링크
+public class backjoon_14889 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        System.out.println();
+        System.out.print("N : " + N);
+
+        int[][] startAndLink = new int[N+1][N+1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 1; j <= N; j++) {
+                startAndLink[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.print(startAndLink[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
