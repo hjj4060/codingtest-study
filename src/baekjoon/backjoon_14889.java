23fe596daeb3ab5a9c9c5be2aec7e5abc02913cf
hjj4060 <rkdgustn77@naver.com>
2023-11-19 02:59:39 +0900

    백준 문제 스타트와 링크

--- src/baekjoon/backjoon_14889.java
+++ src/baekjoon/backjoon_14889.java
@@ -1,5 +1,17 @@
 package baekjoon;
 
-public class backjoon_14889 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
 
+//https://www.acmicpc.net/problem/14889 스타트와 링크
+public class backjoon_14889 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
 }
