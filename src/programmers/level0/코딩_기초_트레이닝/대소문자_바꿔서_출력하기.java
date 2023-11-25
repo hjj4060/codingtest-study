8673954c0e857290a4d8ae68e171ee5d64c76e4d
hjj4060 <rkdgustn77@naver.com>
2023-11-26 00:49:18 +0900

    프로그래머스 레벨0

--- src/programmers/level0/코딩_기초_트레이닝/대소문자_바꿔서_출력하기.java
+++ src/programmers/level0/코딩_기초_트레이닝/대소문자_바꿔서_출력하기.java
@@ -2,26 +2,21 @@ package programmers.level0.코딩_기초_트레이닝;
 
 import java.util.Scanner;
 
-
 //Character.isUpperCase(a) : a값이 대분자면 true
 //Character.toUpperCase(a) : a값을 대문자로 변환
 public class 대소문자_바꿔서_출력하기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
+        String inputString = sc.next();
 
-        char ch[] = a.toCharArray();
+        char ch[] = inputString.toCharArray();
 
         for (char c : ch) {
-            //System.out.println(Character.isUpperCase(c));
-
             if(Character.isUpperCase(c)) {
                 System.out.print(Character.toLowerCase(c));
             }else {
                 System.out.print(Character.toUpperCase(c));
             }
         }
-
-
     }
 }
