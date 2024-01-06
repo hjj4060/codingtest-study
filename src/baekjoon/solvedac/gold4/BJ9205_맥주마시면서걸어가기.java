cc20a85d8056f0f21899878be23583df91aeee92
hjj4060 <rkdgustn77@naver.com>
2024-01-07 03:25:09 +0900

    commit

---
+++ src/baekjoon/solvedac/gold4/BJ9205_맥주마시면서걸어가기.java
@@ -0,0 +1,15 @@
+package baekjoon.solvedac.gold4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ9205_맥주마시면서걸어가기 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st = null;
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+
+    }
+}
---
+++ src/programmers/level0/코딩테스트_입문/겹치는_선분의_길이_refactoring.java
@@ -0,0 +1,26 @@
+package programmers.level0.코딩테스트_입문;
+
+import java.util.HashMap;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120876
+public class 겹치는_선분의_길이_refactoring {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[][] lines = new int[][]{
+                {0, 1},
+                {2, 5},
+                {3, 9}
+        };
+        System.out.println(sol.solution(lines));
+    }
+
+    static class Solution {
+        public int solution(int[][] lines) {
+            int answer = 0;
+
+
+            return answer;
+        }
+    }
+}
