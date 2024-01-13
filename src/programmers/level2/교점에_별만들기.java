361c53a147c86a631f8e7bcf2ff008367d10b88d
hjj4060 <rkdgustn77@naver.com>
2024-01-14 06:03:47 +0900

    commit

---
+++ src/programmers/level2/교점에_별만들기.java
@@ -0,0 +1,20 @@
+package programmers.level2;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/87377
+public class 교점에_별만들기 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        //System.out.println(sol.solution());
+    }
+
+    static class Solution {
+        public String[] solution(int[][] line) {
+            String[] answer = {};
+
+
+
+            return answer;
+        }
+    }
+}
