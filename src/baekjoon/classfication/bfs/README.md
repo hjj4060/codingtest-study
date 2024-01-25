28774a15178720fe567167ebf060632a048f3e92
hjj4060 <rkdgustn77@naver.com>
2024-01-26 00:36:43 +0900

    BFS

---
+++ src/baekjoon/classfication/bfs/README.md
@@ -0,0 +1,3 @@
+최대한 많이 풀은 문제 순서대로 풀기
+
+BFS: https://www.acmicpc.net/problemset?sort=ac_desc&algo=126
---
+++ src/baekjoon/classfication/bfs/미로탐색.java
@@ -0,0 +1,12 @@
+package baekjoon.classfication.bfs;
+
+//https://www.acmicpc.net/problem/2178
+public class 미로탐색 {
+    /**
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
