e7c5474eca77943db8d7df3ea326a2fa251a39ad
HyunSu, Kang <rkdgustn77@naver.com>
2024-04-16 02:05:07 +0900

    commit

---
+++ src/baekjoon/classfication/backtracking/N과M_15649.java
@@ -0,0 +1,38 @@
+package baekjoon.classfication.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//https://www.acmicpc.net/problem/15649
+public class N과M_15649 {
+    /**
+     * 아이디어
+     * 1. 1부터 N중에 하나를 선택한뒤
+     * 2. 다음 1부터 N부터 선택할 때 이미 선택한 값이 아닌 경우 선택
+     * 3. M개를 선택할 경우 프린트
+     *
+     * 시간복잡도(재귀는 2억이 넘지 않아야함)
+     * 1. N^N : 중복이 가능, N = 8 까지 가능
+     * 2. N! : 중복이 불가, N = 10 까지 가능
+     *
+     * 자료구조
+     * 1. 방문 여부 확인 배열 : bool[]
+     * 2. 선택한 값 입력 배열 : int[]
+     */
+
+    static int[] N, M;
+    static boolean[][] isVisited;
+
+    private static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+    }
+}
